e2018f1b85492f7e61e141c33469be1b391aa496
Daniel Jensen <jensendw@gmail.com>
2017-09-20 13:51:49 -0700

    update main

--- main.py
+++ main.py
@@ -23,11 +23,9 @@ def get_health():
     else:
         return jsonify({'status': 'We lost track of the background process'}), 500
 
-'''
+
 if __name__ == "__main__":
     send_events = Process(target=run_loop, args=())
     send_events.start()
     app.run(debug=True, host='0.0.0.0', use_reloader=False)
     send_events.join()
-'''
-get_marathon_services_health(MARATHON_URL)
